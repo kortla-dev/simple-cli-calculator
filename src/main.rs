5dc718be745520979d9e2956a665f46f085b0c05
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2024-07-02 20:02:00 +0200

    parse cli input

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 pub(crate) mod ast;
+pub(crate) mod cli;
 pub(crate) mod lexer;
 
+use std::env;
+
 fn main() {
+    let expression = cli::parse_input(env::args());
     println!("Hello, world!");
 }
