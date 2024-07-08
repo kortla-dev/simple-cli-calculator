8fd9ce1200209d411aee6fb4151d91aa3de4ee2a
Neill Engelbrecht <engelbrecht.neill@gmail.com>
2024-07-08 13:17:29 +0200

    Implemented lexer

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,24 @@
 pub(crate) mod ast;
 pub(crate) mod lexer;
 
+use ast::Token;
+use lexer::Lexer;
+
 fn main() {
+    // let mut inpt = "시험".to_string();
+    let mut inpt = "1+1".to_string();
+
+    let mut lex = Lexer::new(&mut inpt);
+
+    while let Some(tkn) = lex.next() {
+        if tkn == Token::Eos {
+            break;
+        }
+
+        println!("{:?}", tkn);
+    }
+
+    println!("{:?}", lex);
+
     println!("Hello, world!");
 }
